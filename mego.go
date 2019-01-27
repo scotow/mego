3e015864adc898a2318fd45a5ba6437f56d6c8b6
Scotow <contact@scotow.com>
2019-01-27 20:33:33 +0100

    parse file and command line args, refactoring

--- mego.go
+++ mego.go
@@ -1,43 +1,102 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 	"time"
 )
 
-func download(link string) bool {
-	cmd := exec.Command("megadl", "--limit-speed=500", link)
+const (
+	retryInterval = time.Minute
+)
+
+var (
+	speedFlag = flag.Uint("s", 0, "is passed to megadl as --limit-speed")
+	linkRegex = regexp.MustCompile(`^(?:https?://)?mega\.nz/#.+$`)
+)
+
+func isValidLink(link string) bool {
+	return linkRegex.MatchString(link)
+}
+
+func downloadRepeat(link string) {
+	for !downloadCommand(link) {
+		log.Printf("Download of \"%s\" failed, waiting before retrying.\n", link)
+		time.Sleep(retryInterval)
+	}
+
+	log.Printf("Download of \"%s\" done.\n", link)
+}
+
+func downloadCommand(link string) bool {
+	cmd := exec.Command("megadl", fmt.Sprintf("--limit-speed=%d", *speedFlag), link)
 	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
 	return cmd.Run() == nil
 }
 
-func main() {
-	data, err := ioutil.ReadFile(os.Args[1])
+func downloadFromFilesList(path string) {
+	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Fatalln(err)
+		log.Printf("Cannot open file \"%s\". Skipping. (%s)\n", path, err.Error())
 		return
 	}
 
-	links := strings.Split(string(data), "\n")
-	log.Println(len(links))
+	lines := strings.Split(string(data), "\n")
+	links := make([]string, 0, len(lines))
+
+	// Parsing links in file.
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		links = append(links, line)
+	}
+
+	// Download each links in list.
 	for i, link := range links {
 		if link[0] == '#' {
 			log.Printf("Skipping \"%s\"\n", link)
 			continue
 		}
 
-		for !download(link) {
-			log.Printf("Download of \"%s\" failed, waiting before retrying.\n", link)
-			time.Sleep(time.Minute)
+		if !isValidLink(link) {
+			log.Printf("Invalid link %s. Skipping.\n", link)
+			links[i] = fmt.Sprintf("#-%s", link)
+			writeFilesList(path, links)
+			continue
 		}
 
-		links[i] = "#" + link
-		ioutil.WriteFile(os.Args[1], []byte(strings.Join(links, "\n")), 0664)
+		downloadRepeat(link)
+		links[i] = fmt.Sprintf("#%s", link)
+		writeFilesList(path, links)
+	}
+}
+
+func writeFilesList(path string, links []string) {
+	err := ioutil.WriteFile(path, []byte(strings.Join(links, "\n")), 0664)
+	if err != nil {
+		log.Printf("Cannot write file \"%s\". Skipping. (%s)\n", path, err.Error())
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	for _, arg := range flag.Args() {
+		if isValidLink(arg) {
+			downloadRepeat(arg)
+		} else {
+			downloadFromFilesList(arg)
+		}
 	}
 
-	log.Println("Downloads done.")
+	log.Println("All downloads done.")
 }
